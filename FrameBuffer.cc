377f0b5c21123d0adde94f724f5caa918b241608
Richard Bradley <hirdrac@users.noreply.github.com>
2021-07-01 15:11:32 +0900

    fixed filesize in BMP header for non-zero row pad; misc cleanup

--- FrameBuffer.cc
+++ FrameBuffer.cc
@@ -17,35 +17,41 @@
 // Member Functions
 int FrameBuffer::init(int w, int h)
 {
+  if (w <= 0 || h <= 0) { return -1; }
+
   _width = w;
   _height = h;
-  _pixels = w * h;
-  _buffer.resize(std::size_t(_pixels * 3));
+  _buffer.resize(std::size_t(w * h * 3));
   return 0;
 }
 
 int FrameBuffer::saveBMP(const std::string& filename) const
 {
-  int file_size = 54 + (_pixels * 3);
+  if (_buffer.empty()) { return -1; }
 
-  // Set up 24bit BMP header
+  const int row_pad = (4 - ((_width * 3) % 4)) % 4;
+    // rows must be padded to 4-byte multiple
+  const int row_size = (_width * 3) + row_pad;
+  const uint32_t file_size = 54 + (row_size * _height);
+
+  // setup BMP header
   uint8_t header[54] = {};
-  header[ 0] = 66;                               // BMP type
+  header[ 0] = 66;                               // BMP type "BM"
   header[ 1] = 77;
   header[ 2] = uint8_t(file_size         & 255); // File size
   header[ 3] = uint8_t((file_size >>  8) & 255);
   header[ 4] = uint8_t((file_size >> 16) & 255);
-  header[ 5] = uint8_t((file_size >> 24) & 255);
+  header[ 5] = uint8_t(file_size >> 24);
   header[10] = 54;                               // Bitmap start
   header[14] = 40;                               // BITMAPINFOHEADER size
   header[18] = uint8_t(_width          & 255);   // BMP width
   header[19] = uint8_t((_width  >>  8) & 255);
   header[20] = uint8_t((_width  >> 16) & 255);
-  header[21] = uint8_t((_width  >> 24) & 255);
+  header[21] = uint8_t(_width  >> 24);
   header[22] = uint8_t(_height         & 255);   // BMP height
   header[23] = uint8_t((_height >>  8) & 255);
   header[24] = uint8_t((_height >> 16) & 255);
-  header[25] = uint8_t((_height >> 24) & 255);
+  header[25] = uint8_t(_height >> 24);
   header[26] = 1;                                // Planes
   header[28] = 24;                               // Bit count
 
@@ -58,11 +64,7 @@ int FrameBuffer::saveBMP(const std::string& filename) const
 
   file.write(reinterpret_cast<char*>(header), sizeof(header));
 
-  int row_pad = (4 - ((_width * 3) % 4)) % 4;
-    // rows must be padded to 4-byte multiple
-  int row_size = (_width * 3) + row_pad;
   auto row = std::make_unique<uint8_t[]>(std::size_t(row_size));
-
   auto src = _buffer.begin();
   for (int y = 0; y < _height; ++y) {
     uint8_t* out = row.get();
@@ -81,7 +83,7 @@ int FrameBuffer::saveBMP(const std::string& filename) const
   return 0;
 }
 
-int FrameBuffer::clear(float r, float g, float b)
+void FrameBuffer::clear(float r, float g, float b)
 {
   auto itr = _buffer.begin(), end = _buffer.end();
   while (itr != end) {
@@ -89,14 +91,11 @@ int FrameBuffer::clear(float r, float g, float b)
     *itr++ = g;
     *itr++ = b;
   }
-  return 0;
 }
 
 int FrameBuffer::plot(int x, int y, float r, float g, float b)
 {
-  if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height)) {
-    return -1;
-  }
+  if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height)) { return -1; }
 
   float* ptr = &_buffer[std::size_t(((y * _width) + x) * 3)];
   *ptr++ = r;
@@ -147,9 +146,7 @@ int FrameBuffer::line(int x0, int y0, int x1, int y1,
 
 int FrameBuffer::value(int x, int y, float& r, float& g, float& b) const
 {
-  if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height)) {
-    return -1;
-  }
+  if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height)) { return -1; }
 
   const float* ptr = &_buffer[std::size_t(((y * _width) + x) * 3)];
   r = *ptr++;
@@ -160,9 +157,7 @@ int FrameBuffer::value(int x, int y, float& r, float& g, float& b) const
 
 int FrameBuffer::range(float& min_val, float& max_val) const
 {
-  if (_buffer.empty()) {
-    return -1;
-  }
+  if (_buffer.empty()) { return -1; }
 
   auto itr = _buffer.begin(), end = _buffer.end();
   float low  = *itr;
--- FrameBuffer.hh
+++ FrameBuffer.hh
@@ -25,7 +25,7 @@ class FrameBuffer
   int init(int width, int height);
   int saveBMP(const std::string& filename) const;
 
-  int clear(float r = 0, float g = 0, float b = 0);
+  void clear(float r = 0, float g = 0, float b = 0);
   int plot(int x, int y, float r, float g, float b);
   int line(int x0, int y0, int x1, int y1, float r, float g, float b);
   int value(int x, int y, float& r, float& g, float& b) const;
@@ -35,7 +35,7 @@ class FrameBuffer
   [[nodiscard]] int height() const { return _height; }
 
  private:
-  int _width = 0, _height = 0, _pixels = 0;
+  int _width = 0, _height = 0;
   std::vector<float> _buffer;
 
   // prevent copy/assign
