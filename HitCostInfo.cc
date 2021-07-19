e92a4fb0b43979a4e50c3bb7238812a6aaa4a56e
Richard Bradley <hirdrac@users.noreply.github.com>
2021-07-19 20:35:14 +0900

    hit cost changes to improve bound creation

--- HitCostInfo.cc
+++ HitCostInfo.cc
@@ -16,14 +16,14 @@ void HitCostInfo::reset()
 {
   bound         = .5;
   disc          = .75;
-  cone          = 1.2;
-  csg           = .2;
-  cube          = 1.5;
-  cylinder      = 1.4;
-  open_cone     = 1.0;
-  open_cylinder = 1.0;
-  paraboloid    = 1.0;
+  cone          = 1.6;
+  csg           = 1.0;
+  cube          = 1.2;
+  cylinder      = 1.8;
+  open_cone     = 1.3;
+  open_cylinder = 1.3;
+  paraboloid    = 1.3;
   plane         = .75;
   sphere        = 1.0;
-  torus         = 3.0;
+  torus         = 15.0;
 }
