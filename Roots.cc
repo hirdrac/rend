8a5010d53a5c17530987a6faffd28d88c2ba8775
Richard Bradley <hirdrac@users.noreply.github.com>
2021-07-21 04:06:23 +0900

    cleanup

--- Roots.cc
+++ Roots.cc
@@ -3,7 +3,7 @@
 // Copyright (C) 2021 Richard Bradley
 //
 // Implementation of polynomial root finding functions
-// Most of SolveQuadric(), SolveCubic(), SolveQuartic() taken from code
+// SolveQuadric(), SolveCubic(), SolveQuartic() based on code
 // by Jochen Schwarze (Graphics Gems I)
 //
 
@@ -13,7 +13,7 @@
 
 
 // **** Inlined Functions ****
-static constexpr Flt CBRT(Flt x)
+static inline Flt CBRT(Flt x)
 {
   if (x > 0.0) {
     return std::pow(x, 1.0/3.0);
@@ -29,16 +29,16 @@ static constexpr Flt CBRT(Flt x)
 int SolveQuadric(const Flt c[3], Flt s[2])
 {
   // normal form: x^2 + px + q = 0
-  Flt p = c[1] / (2 * c[2]);
-  Flt q = c[0] / c[2];
-  Flt d = Sqr(p) - q;
+  const Flt p = c[1] / (2 * c[2]);
+  const Flt q = c[0] / c[2];
+  const Flt d = Sqr(p) - q;
 
   if (IsNegative(d)) {
     // no solutions
     return 0;
   } else if (IsPositive(d)) {
     // two solutions
-    Flt sqrt_d = std::sqrt(d);
+    const Flt sqrt_d = std::sqrt(d);
     s[0] = -p - sqrt_d;
     s[1] = -p + sqrt_d;
     return 2;
@@ -53,33 +53,33 @@ int SolveQuadric(const Flt c[3], Flt s[2])
 int SolveCubic(const Flt c[4], Flt s[3])
 {
   // normal form: x^3 + Ax^2 + Bx + C = 0
-  Flt A = c[2] / c[3];
-  Flt B = c[1] / c[3];
-  Flt C = c[0] / c[3];
+  const Flt A = c[2] / c[3];
+  const Flt B = c[1] / c[3];
+  const Flt C = c[0] / c[3];
 
   // substitute x = y - A/3 to eliminate quadric term: x^3 +px + q = 0
-  Flt sq_A = Sqr(A);
-  Flt third_A = A / 3.0;
-  Flt p = (-1.0/3.0 * sq_A + B) / 3.0;
-  Flt q = .5 * (2.0/27.0 * A * sq_A - third_A * B + C);
+  const Flt sq_A = Sqr(A);
+  const Flt third_A = A / 3.0;
+  const Flt p = (-1.0/3.0 * sq_A + B) / 3.0;
+  const Flt q = .5 * (2.0/27.0 * A * sq_A - third_A * B + C);
 
   // use Cardano's formula
-  Flt cb_p = Sqr(p) * p;
-  Flt D = Sqr(q) + cb_p;
+  const Flt cb_p = Sqr(p) * p;
+  const Flt D = Sqr(q) + cb_p;
 
   if (IsNegative(D)) {
     // three real solutions
-    Flt phi = std::acos(-q / std::sqrt(-cb_p)) / 3.0;
-    Flt t = 2 * std::sqrt(-p);
+    const Flt phi = std::acos(-q / std::sqrt(-cb_p)) / 3.0;
+    const Flt t = 2 * std::sqrt(-p);
     s[0] = (t * std::cos(phi)) - third_A;
     s[1] = (-t * std::cos(phi + (PI / 3))) - third_A;
     s[2] = (-t * std::cos(phi - (PI / 3))) - third_A;
     return 3;
   } else if (IsPositive(D)) {
     // one real solution
-    Flt sqrt_D = std::sqrt(D);
-    Flt u =  CBRT(sqrt_D - q);
-    Flt v = -CBRT(sqrt_D + q);
+    const Flt sqrt_D = std::sqrt(D);
+    const Flt u =  CBRT(sqrt_D - q);
+    const Flt v = -CBRT(sqrt_D + q);
     s[0] = (u + v) - third_A;
     return 1;
   } else if (IsZero(q)) {
@@ -88,7 +88,7 @@ int SolveCubic(const Flt c[4], Flt s[3])
     return 1;
   } else {
     // one single and one double solution
-    Flt u = CBRT(-q);
+    const Flt u = CBRT(-q);
     s[0] = (2 * u)- third_A;
     s[1] = -u - third_A;
     return 2;
@@ -99,33 +99,33 @@ int SolveCubic(const Flt c[4], Flt s[3])
 static int solveCubic1only(const Flt c[4], Flt* s)
 {
   // normal form: x^3 + Ax^2 + Bx + C = 0
-  Flt A = c[2] / c[3];
-  Flt B = c[1] / c[3];
-  Flt C = c[0] / c[3];
+  const Flt A = c[2] / c[3];
+  const Flt B = c[1] / c[3];
+  const Flt C = c[0] / c[3];
 
   // substitute x = y - A/3 to eliminate quadric term: x^3 +px + q = 0
-  Flt sq_A = Sqr(A);
-  Flt third_A = A / 3.0;
-  Flt p = (-1.0/3.0 * sq_A + B) / 3.0;
-  Flt q = .5 * (2.0/27.0 * A * sq_A - third_A * B + C);
+  const Flt sq_A = Sqr(A);
+  const Flt third_A = A / 3.0;
+  const Flt p = (-1.0/3.0 * sq_A + B) / 3.0;
+  const Flt q = .5 * (2.0/27.0 * A * sq_A - third_A * B + C);
 
   // use Cardano's formula
-  Flt cb_p = Sqr(p) * p;
-  Flt D = Sqr(q) + cb_p;
+  const Flt cb_p = Sqr(p) * p;
+  const Flt D = Sqr(q) + cb_p;
 
   if (IsNegative(D)) {
     // three real solutions
-    Flt phi = std::acos(-q / std::sqrt(-cb_p)) / 3.0;
-    Flt t = 2 * std::sqrt(-p);
+    const Flt phi = std::acos(-q / std::sqrt(-cb_p)) / 3.0;
+    const Flt t = 2 * std::sqrt(-p);
     s[0] = (t * std::cos(phi)) - third_A;
     //s[1] = (-t * std::cos(phi + (PI / 3))) - third_A;
     //s[2] = (-t * std::cos(phi - (PI / 3))) - third_A;
     return 1; // 3
   } else if (IsPositive(D)) {
     // one real solution
-    Flt sqrt_D = std::sqrt(D);
-    Flt u =  CBRT(sqrt_D - q);
-    Flt v = -CBRT(sqrt_D + q);
+    const Flt sqrt_D = std::sqrt(D);
+    const Flt u =  CBRT(sqrt_D - q);
+    const Flt v = -CBRT(sqrt_D + q);
     s[0] = (u + v) - third_A;
     return 1;
   } else if (IsZero(q)) {
@@ -134,7 +134,7 @@ static int solveCubic1only(const Flt c[4], Flt* s)
     return 1;
   } else {
     // one single and one double solution
-    Flt u = CBRT(-q);
+    const Flt u = CBRT(-q);
     s[0] = (2 * u) - third_A;
     //s[1] = -u - third_A;
     return 1; // 2
@@ -145,17 +145,18 @@ static int solveCubic1only(const Flt c[4], Flt* s)
 int SolveQuartic(const Flt c[5], Flt s[4])
 {
   // normal form: x^4 + Ax^3 + Bx^2 + Cx + D = 0
-  Flt A = c[3] / c[4];
-  Flt B = c[2] / c[4];
-  Flt C = c[1] / c[4];
-  Flt D = c[0] / c[4];
+  const Flt A = c[3] / c[4];
+  const Flt B = c[2] / c[4];
+  const Flt C = c[1] / c[4];
+  const Flt D = c[0] / c[4];
 
   // substitute x = y - A/4 to eliminate cubic term: x^4 + px^2 + qx + r = 0
-  Flt sq_A = Sqr(A);
-  Flt qtr_A = .25 * A;
-  Flt p = -.375 * sq_A + B;
-  Flt q =  .125 * sq_A * A - .5 * A * B + C;
-  Flt r = (-3.0/256.0) * sq_A * sq_A + (1.0/16.0) * sq_A * B - qtr_A * C + D;
+  const Flt sq_A = Sqr(A);
+  const Flt qtr_A = .25 * A;
+  const Flt p = -.375 * sq_A + B;
+  const Flt q =  .125 * sq_A * A - .5 * A * B + C;
+  const Flt r = (-3.0/256.0) * sq_A * sq_A
+    + (1.0/16.0) * sq_A * B - qtr_A * C + D;
 
   Flt coeffs[4];
   int num;
@@ -178,7 +179,7 @@ int SolveQuartic(const Flt c[5], Flt s[4])
     if (solveCubic1only(coeffs, s) == 0) { return 0; }
 
     // take the one real solution to build two quadric equations
-    Flt z = s[0];
+    const Flt z = s[0];
 
     Flt u = Sqr(z) - r;
     if (IsNegative(u)) { return 0; }
