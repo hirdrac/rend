7badf7b4358fad80c6b20601b1d4d0c97309a046
Richard Bradley <hirdrac@users.noreply.github.com>
2024-05-03 04:40:39 +0900

    cleanup

--- src/SList.hh
+++ src/SList.hh
@@ -1,6 +1,6 @@
 //
 // SList.hh
-// Copyright (C) 2023 Richard Bradley
+// Copyright (C) 2024 Richard Bradley
 //
 // Intrusive single linked list
 //
@@ -18,24 +18,26 @@
 
 
 // **** SList class ****
-template<ListNode type>
+template<ListNode T>
 class SList
 {
  public:
+  using type = SList<T>;
+
   SList() = default;
-  SList(type* item) : _head{item}, _tail{LastNode(item)} { }
+  explicit SList(T* item) : _head{item}, _tail{LastNode(item)} { }
   ~SList() { KillNodes(_head); }
 
   // prevent copy/assign
-  SList(const SList<type>&) = delete;
-  SList<type>& operator=(const SList<type>&) = delete;
+  SList(const type&) = delete;
+  type& operator=(const type&) = delete;
 
   // allow move/move-assign
-  SList(SList<type>&& x) noexcept :
+  SList(type&& x) noexcept :
     _head{std::exchange(x._head, nullptr)},
     _tail{std::exchange(x._tail, nullptr)} { }
 
-  SList<type>& operator=(SList<type>&& x) noexcept {
+  type& operator=(type&& x) noexcept {
     if (this != &x) {
       KillNodes(_head);
       _head = std::exchange(x._head, nullptr);
@@ -46,63 +48,63 @@ class SList
 
 
   // Member Functions
-  [[nodiscard]] type* head() { return _head; }
-  [[nodiscard]] const type* head() const { return _head; }
+  [[nodiscard]] T* head() { return _head; }
+  [[nodiscard]] const T* head() const { return _head; }
 
-  [[nodiscard]] type* tail() { return _tail; }
-  [[nodiscard]] const type* tail() const { return _tail; }
+  [[nodiscard]] T* tail() { return _tail; }
+  [[nodiscard]] const T* tail() const { return _tail; }
 
   [[nodiscard]] bool empty() const { return !_head; }
   [[nodiscard]] int count() const { return CountNodes(_head); }
 
   void purge() { KillNodes(_head); _head = _tail = nullptr; }
 
-  void addToHead(type* item);
-  void addToHead(SList<type>& list);
-  void addToTail(type* item);
-  void addToTail(SList<type>& list);
-  void addAfterNode(type* node, type* item);
+  void addToHead(T* item);
+  void addToHead(type& list);
+  void addToTail(T* item);
+  void addToTail(type& list);
+  void addAfterNode(T* node, T* item);
 
-  type* removeHead();
-  type* removeNext(type* item);
-  type* extractNodes();
+  T* removeHead();
+  T* removeNext(T* item);
+  T* extractNodes();
 
-  void swap(SList<type>& x) noexcept {
+  void swap(type& x) noexcept {
     std::swap(_head, x._head);
     std::swap(_tail, x._tail);
   }
 
  private:
-  type* _head = nullptr;
-  type* _tail = nullptr;
+  T* _head = nullptr;
+  T* _tail = nullptr;
 };
 
-template<ListNode type>
-void SList<type>::addToHead(type* item)
+template<ListNode T>
+void SList<T>::addToHead(T* item)
 {
   //assert(item != nullptr);
-  type* end = LastNode(item);
+  T* end = LastNode(item);
   end->next = _head;
   if (!_tail) { _tail = end; }
 
   _head = item;
 }
 
-template<ListNode type>
-void SList<type>::addToHead(SList<type>& list)
+template<ListNode T>
+void SList<T>::addToHead(type& list)
 {
   if (list.empty()) { return; }
 
-  type* end = list.tail();
-  type* start = list.extractNodes();
+  T* end = list.tail();
+  T* start = list.extractNodes();
   end->next = _head;
   if (!_tail) { _tail = end; }
 
   _head = start;
 }
 
-template<ListNode type>
-void SList<type>::addToTail(type* item)
+template<ListNode T>
+void SList<T>::addToTail(T* item)
 {
   //assert(item != nullptr);
   if (_tail) {
@@ -114,13 +116,13 @@ void SList<type>::addToTail(type* item)
   _tail = LastNode(item);
 }
 
-template<ListNode type>
-void SList<type>::addToTail(SList<type>& list)
+template<ListNode T>
+void SList<T>::addToTail(type& list)
 {
   if (list.empty()) { return; }
 
-  type* end = list.tail();
-  type* start = list.extractNodes();
+  T* end = list.tail();
+  T* start = list.extractNodes();
 
   if (_tail) {
     _tail->next = start;
@@ -131,8 +133,8 @@ void SList<type>::addToTail(SList<type>& list)
   _tail = end;
 }
 
-template<ListNode type>
-void SList<type>::addAfterNode(type* node, type* item)
+template<ListNode T>
+void SList<T>::addAfterNode(T* node, T* item)
 {
   if (!node) {
     addToHead(item);
@@ -145,12 +147,12 @@ void SList<type>::addAfterNode(type* node, type* item)
   }
 }
 
-template<ListNode type>
-type* SList<type>::removeHead()
+template<ListNode T>
+T* SList<T>::removeHead()
 {
   if (!_head) { return nullptr; }
 
-  type* n = _head;
+  T* n = _head;
   _head = _head->next;
   if (!_head) { _tail = nullptr; }
 
@@ -158,12 +160,12 @@ type* SList<type>::removeHead()
   return n;
 }
 
-template<ListNode type>
-type* SList<type>::removeNext(type* item)
+template<ListNode T>
+T* SList<T>::removeNext(T* item)
 {
   if (!item) { return removeHead(); }
 
-  type* n = item->next;
+  T* n = item->next;
   if (n) {
     item->next = n->next;
     if (_tail == n) { _tail = item; }
@@ -174,15 +176,15 @@ type* SList<type>::removeNext(type* item)
   return n;
 }
 
-template<ListNode type>
-type* SList<type>::extractNodes()
+template<ListNode T>
+T* SList<T>::extractNodes()
 {
-  type* list = _head;
+  T* list = _head;
   _head = _tail = nullptr;
   return list;
 }
 
 
 // **** Functions ****
-template<ListNode type>
-void swap(SList<type>& a, SList<type>& b) noexcept { a.swap(b); }
+template<ListNode T>
+void swap(SList<T>& a, SList<T>& b) noexcept { a.swap(b); }
